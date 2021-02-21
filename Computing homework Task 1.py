5b710d941d52038ef4826c720d907898119d344f
EnglebertCardenoOPHS <71763263+EnglebertCardenoOPHS@users.noreply.github.com>
2021-02-21 17:11:27 +0000

    Add files via upload

---
+++ Computing homework Task 1.py
@@ -0,0 +1,16 @@
+#TASK 1
+
+print(" Enter your personal details down below->")
+
+#CODE ASKING USER TO ENTER PERSONAL DETAILS
+FirstName = str(input("Enter your first name-->"))
+LastName = str(input("Enter your last name-->"))
+Age = int(input("Enter your age-->"))
+Address = input("Enter your address-->")
+
+#CODE DISPLAYING PLAYERS PERSONAL DETAILS
+print("Your first name is",FirstName,"and your last name is",LastName,"."
+      "Your age is",Age,"and you live on",Address,".")
+
+
+
